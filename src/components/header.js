fd091bc55f7fab9e909cd7f596e6f678e6bbd0be
jumpvin <jumpvince@gmail.com>
2019-11-11 20:36:06 +0100

    feat: responsive nav menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,24 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import './styles/header.css';
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle }) => {
+  const [ windowSize ] = useState({ width: window.innerWidth, height: window.innerHeight });
+  const [ showSub, setShowSub ] = useState(false);
+  console.log(windowSize);
+
+  const menuType = () => {
+    return windowSize.width <= 650 ? true : false;
+  }
+
+  const handleShowSub = () => {
+    setShowSub(showSub ? false : true);
+  };
+
+
+
+  return (
   <header>
     <div className="logo">
       <h1>
@@ -13,16 +28,27 @@ const Header = ({ siteTitle }) => (
       </h1>
     </div>
     <div className="menu">
-      <Link to="/page-2/">
-          Blog
-      </Link>
-      <Link to="/about/">
-          About
-      </Link>
-      <a href = 'https://demo.doropomo.com'> Demo </a>
+      <div 
+        className = { menuType() ? 'hamburger show-burger' : 'hamburger' }
+        onClick = { handleShowSub }
+      >
+        <div className = 'menu-line' ></div>
+        <div className = 'menu-line' ></div>
+        <div className = 'menu-line' ></div>
+      </div>
+      <div className = { menuType() ? showSub ? 'menu-items sub-menu show-sub-menu' : 'menu-items sub-menu' : 'menu-items' } >
+        <Link to="/page-2/">
+            Blog
+        </Link>
+        <Link to="/about/">
+            About
+        </Link>
+        <a href = 'https://demo.doropomo.com'> Demo </a>
+      </div>
     </div>
   </header>
-)
+  )
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
