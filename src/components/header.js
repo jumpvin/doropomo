9c715fea5b93adb424542ef20e98bfbcff7afd6f
jumpvin <jumpvince@gmail.com>
2019-11-12 18:00:20 +0100

    bug: window wasn't passing correct values

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,42 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import './styles/header.css';
 
 const Header = ({ siteTitle }) => {
   const info = typeof window !== `undefined` ? {width: window.innerWidth, height: window.innerHeight } : { width: '', height: '' };
   const [ windowSize ] = useState(info);
   const [ showSub, setShowSub ] = useState(false);
+  const [menuType, setMenuType ] = useState(false);
+  console.log(info);
 
-  const menuType = () => {
-    return windowSize.width <= 650 ? true : false;
-  }
+  useEffect( () => {
+    setMenuType(windowSize.width <= 650 ? true : false);
+  }, [windowSize]);
 
   const handleShowSub = () => {
     setShowSub(showSub ? false : true);
   };
 
-
-
   return (
   <header>
-    <div className="logo">
+    <div className = 'logo'>
       <h1>
         <Link to="/">
           {siteTitle}
         </Link>
       </h1>
     </div>
-    <div className="menu">
+    <div className= 'menu'>
       <div 
-        className = { menuType() ? 'hamburger show-burger' : 'hamburger' }
+        className = { menuType ? 'hamburger show-burger' : 'hamburger' }
         onClick = { handleShowSub }
       >
         <div className = 'menu-line' ></div>
         <div className = 'menu-line' ></div>
         <div className = 'menu-line' ></div>
       </div>
-      <div className = { menuType() ? showSub ? 'menu-items sub-menu show-sub-menu' : 'menu-items sub-menu' : 'menu-items' } >
+      <div className = { menuType ? showSub ? 'menu-items sub-menu show-sub-menu' : 'menu-items sub-menu' : 'menu-items' } >
         <Link to="/page-2/">
             Blog
         </Link>
